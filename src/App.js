d298afbc5f5a0f840d35382a36e7a2723bfff868
Yevhenii Kozarenko <kozarenk007@gmail.com>
2020-05-18 21:54:01 +0300

    befor social network project start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import Technologies from "Technologies.js";
-import Header from "Header";
+import Technologies from "./Technologies.js";
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
       <Header />
       <Technologies />
+      <Footer />
     </div>
   )
 };
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      Some footer
+    </div>
+  )
+};
+
+export default Footer;
